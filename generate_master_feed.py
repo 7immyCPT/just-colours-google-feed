9ad9a0db985c93b176132765a218c08480821ad2
7immyCPT <by.cloete@gmail.com>
2026-04-23 11:46:14 +0200

    feat: add XSL stylesheet for browser-friendly XML viewingAll

--- generate_master_feed.py
+++ generate_master_feed.py
@@ -191,7 +191,7 @@ def write(rss, path):
     tree = ET.ElementTree(rss)
     with open(path, "w", encoding="utf-8") as f:
         f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-                f.write('<?xml-stylesheet type="text/xsl" href="feed.xsl"?>\n')
+        f.write('<?xml-stylesheet type="text/xsl" href="feed.xsl"?>\n')
         tree.write(f, encoding="unicode", xml_declaration=False)
 
 def main():
